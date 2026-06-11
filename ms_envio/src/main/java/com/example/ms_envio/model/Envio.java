1590a121532123e0774fd981812de6bfd0c46cf9
Axel-juris <ax.juris@duocuc.cl>
2026-06-11 13:16:45 -0400

    Validacion de envio

--- ms_envio/src/main/java/com/example/ms_envio/model/Envio.java
+++ ms_envio/src/main/java/com/example/ms_envio/model/Envio.java
@@ -9,6 +9,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -26,12 +29,18 @@ public class Envio {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "La dirección de destino es obligatoria")
+    @Size(min = 5, max = 100, message = "La dirección debe tener entre 5 y 100 caracteres")
     private String direccionDestino;
 
+    @NotBlank(message = "El nombre del cliente es obligatorio")
+    @Size(min = 3, max = 50, message = "El nombre del cliente debe tener entre 3 y 50 caracteres")
     private String cliente;
 
+    @NotNull(message = "La fecha de envío es obligatoria")
     private LocalDateTime fechaEnvio;
 
+    @NotNull(message = "El estado del envío es obligatorio")
     @Enumerated(EnumType.STRING)
     private EstadoEnvio estado;
 }
