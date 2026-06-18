49f98b0dbc71d601b0a59abe0be5f0b43f2b6a2f
Axel-juris <ax.juris@duocuc.cl>
2026-06-18 12:50:30 -0400

    Tiene los 4 metodos

--- ms_proveedores/src/main/java/com/example/ms_proveedores/dto/AbastecimientoUpdateDto.java
+++ ms_proveedores/src/main/java/com/example/ms_proveedores/dto/AbastecimientoUpdateDto.java
@@ -1,7 +1,9 @@
 package com.example.ms_proveedores.dto;
 
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -9,6 +11,11 @@ public class AbastecimientoUpdateDto {
 
     @NotNull(message = "El proveedorId es obligatorio")
     private Long proveedorId;
+
+
+    @NotBlank(message = "El nombre del proveedor es obligatorio")
+    @Size(min = 2, max = 80, message = "El nombre debe tener entre 2 y 80 caracteres")
+    private String nombreProveedor;
 
     @NotNull(message = "El productoId es obligatorio")
     private Long productoId;
