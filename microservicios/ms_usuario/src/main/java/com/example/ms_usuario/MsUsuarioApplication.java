44658d54e657932738551f1d7ed055c73ba17b35
IgnacioEspinoza333 <ign.espinozae@duocuc.cl>
2026-06-21 15:46:51 -0400

    eureka

--- microservicios/ms_usuario/src/main/java/com/example/ms_usuario/MsUsuarioApplication.java
+++ microservicios/ms_usuario/src/main/java/com/example/ms_usuario/MsUsuarioApplication.java
@@ -2,12 +2,12 @@ package com.example.ms_usuario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsUsuarioApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MsUsuarioApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(MsUsuarioApplication.class, args);
+    }
 }
